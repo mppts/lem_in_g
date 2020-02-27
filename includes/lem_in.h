d0468319d7ff8d4fe81f85b6e4c16e76664ac395
dorphan <egor.shvecov@yandex.ru>
2020-02-27 21:37:08 +0300

    norminetted header

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -21,22 +21,19 @@
 
 # define FALSE 0
 # define TRUE 1
-# define WHITE 0
-# define GRAY 1
-# define BLACK 2
 
 typedef struct		s_r_reader
 {
-	char 			*name_room;
-	char 			*sx;
-	char 			*sy;
-	char 			*from;
-	char 			*to;
+	char			*name_room;
+	char			*sx;
+	char			*sy;
+	char			*from;
+	char			*to;
 }					t_r_reader;
 
 typedef struct		s_path
 {
-	int 			id;
+	int				id;
 	uint64_t		len;
 	uint64_t		ants;
 	struct s_room	**path;
@@ -46,13 +43,13 @@ typedef struct		s_path
 typedef struct		s_way
 {
 	int				way_number;
-	int 			way_len;
+	int				way_len;
 	struct s_room	*room;
 	struct s_way	*next;
 	struct s_way	*prev_way;
 }					t_way;
 
-typedef struct 		s_graph_inf
+typedef struct		s_graph_inf
 {
 	struct s_way	**ways;
 	struct s_room	**common_rooms;
@@ -60,7 +57,7 @@ typedef struct 		s_graph_inf
 	int64_t			len_t;
 	int64_t			enough_c;
 	int64_t			enough_n;
-	int 			max_ways;
+	int				max_ways;
 }					t_graph_inf;
 
 typedef struct		s_link
@@ -77,7 +74,7 @@ typedef struct		s_link
 */
 typedef struct		s_room
 {
-	int 			ant;
+	int				ant;
 	char			*name;
 	int64_t			level;
 	int64_t			level_rev;
@@ -108,9 +105,9 @@ typedef struct		s_map
 	uint8_t			is_rooms_hashed;
 	uint64_t		len_rh;
 	t_room			**hashed_rooms;
-	char 			*buf;
-	char 			*out;
-	t_dstr 			*dstr;
+	char			*buf;
+	char			*out;
+	t_dstr			*dstr;
 	struct s_path	*path;
 }					t_map;
 
@@ -130,7 +127,7 @@ typedef struct		s_deq
 typedef struct		s_positioner
 {
 	uint64_t		offset;
-	t_path 			*path;
+	t_path			*path;
 }					t_positioner;
 /*
 ** parse_map.c
@@ -147,7 +144,6 @@ void				init_room(t_room *new, int x, int y, char *name);
 ** parse_links.c
 */
 void				add_link(char *buf, t_map *map);
-
 /*
 ** utils_utils.c
 */
@@ -163,16 +159,13 @@ t_room				*find_hashed_room(t_map *map, char *name);
 */
 void				del_map(t_map *map);
 void				del_room(t_room *room);
-
 void				man_err_map(char *msg, char **data,
 					void (*f_todel)(char**), t_map *map);
-
 /*
 ** solver.c
 */
 void				solver(t_map *map);
 void				free_way(t_way *way);
-
 /*
 ** solver_initialization.c
 */
@@ -181,27 +174,14 @@ void				initialization(t_map *map, t_graph_inf	*inf);
 ** solver_algorithm.c
 */
 void				algo(t_map *map, t_graph_inf *inf);
-
-/*
-** solver_algorithm_cleaning.c
-*/
-void		delite_mirror_links(t_graph_inf *inf);
-void		clean_ways(t_room ***ways);
-void		clean_levels_and_flows(t_room *source);
-t_graph_inf	*time_to_do_some_cleaning(t_map *map, t_graph_inf *inf);
-
-
-/*
-** solver_bfs.c
-*/
-int				bellman_ford(t_map *map, t_room **line, t_graph_inf *inf);
-int				bellman_ford_rev(t_map *map, t_room **line, t_graph_inf *inf);
-
 /*
 ** solver_find_best_current_way.c
 */
-int		find_best_current_way(t_map *map, t_room **way_1, t_room **way_2, t_graph_inf *inf);
-
+int					find_best_current_way(t_map *map, t_room **way_1,
+											t_room **way_2, t_graph_inf *inf);
+int					bellman_ford(t_map *map, t_room **line, t_graph_inf *inf);
+int					bellman_ford_rev(t_map *map, t_room **line,
+										t_graph_inf *inf);
 /*
 ** solver_bf_find_way.c
 */
@@ -210,19 +190,17 @@ int					find_way_bf(t_map *map, t_room **line);
 ** solver_bf_es_find_way.c
 */
 int					find_way_bf_e_s(t_map *map, t_room **line);
-
 /*
 ** solver_auxiliary_works_algo.c
 */
-void				auxiliary_works(t_map *map, t_graph_inf *inf, t_room **way_1);
-
+void				auxiliary_works(t_map *map,
+									t_graph_inf *inf, t_room **way_1);
 /*
 ** solver_auxiliary_works_algo_2.c
 */
 void				sort_ways(t_graph_inf *inf);
 void				give_right_numbers_to_ways(t_graph_inf *inf);
 void				clear_common_rooms(t_graph_inf *inf);
-
 /*
 ** solver_find_way.c
 */
@@ -231,17 +209,17 @@ void				find_way_1(t_map *map, t_room **line, t_graph_inf *inf);
 ** solver_separation.c
 */
 void				separate_cross_ways(t_graph_inf *inf, t_map *map);
-
 /*
 ** solver_sufficienty_c.c
 */
 int					sufficienty_c(t_graph_inf *inf, t_map *map);
-
 /*
 ** solver_sufficienty_c_common_rooms_calc.c
 */
 int					common_rooms_calc(t_graph_inf *inf);
-
+/*
+** writer_main.c
+*/
 void				writer(t_map *map);
 
 #endif

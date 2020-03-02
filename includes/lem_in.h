7874c8bd2d61a7809eefb3f8c75383d4cb6fa1b8
dorphan <egor.shvecov@yandex.ru>
2020-03-02 17:56:22 +0300

    small bug fixed

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: limry <limry@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 12:03:07 by limry             #+#    #+#             */
-/*   Updated: 2020/03/02 12:42:19 by limry            ###   ########.fr       */
+/*   Updated: 2020/03/02 17:39:03 by dorphan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,7 @@ typedef struct		s_map
 	char			*buf;
 	t_dstr			*dstr;
 	struct s_path	*path;
-	char			cycle_found;
+	int				cycle_found;
 }					t_map;
 
 typedef struct		s_flag
